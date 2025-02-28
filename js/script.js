645432c4a45e665618e5699a24581c4dfed3891e
Hampiz <hampus.bengtsson@elev.ga.ntig.se>
2025-02-28 15:10:59 +0100

    Added some pictures, made a new html file, made some small changes to make the website better and more easy to use

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ document.addEventListener("DOMContentLoaded", function() {
         navList.classList.toggle("active");
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 300) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
+        }
+    });
+
+    scrollToTopBtn.addEventListener("click", function () {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
